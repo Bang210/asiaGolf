18d8055e6cb3034340d2d0a9575dc36729371309
Bang210 <bangvelope@gmail.com>
2025-03-25 21:34:14 +0900

    init

---
+++ src/main/java/kdd/toy/asiagolf/AsiaGolfApplication.java
@@ -0,0 +1,13 @@
+package kdd.toy.asiagolf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsiaGolfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsiaGolfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kdd/toy/asiagolf/AsiaGolfApplicationTests.java
@@ -0,0 +1,13 @@
+package kdd.toy.asiagolf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsiaGolfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
